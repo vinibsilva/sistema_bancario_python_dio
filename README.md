130714206775e775818cc21db24c1b7034eab73f
Vinícius <vinibsilva32@gmail.com>
2024-04-27 12:18:59 -0300

    Atualização do Readme e conclusão do sistema bancário

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# Primeiro projeto: Sistema bancário
+
+Esse é o primeiro desafio de código proposto para os estudantes do curso de Python BackEnd. O sistema é simples mas requer algumas restrições. Abaixo seguem alguns dos requisitos desse sistema.
+
+# Requisitos do sistema
+1. O sistema deve ter operações de depósito, saque e extrato.
+2. O usuário deve fazer depósitos e esses depósitos devem ser armazenados em uma variável estar presentes no extrato.
+3. O usuário só pode realizar 3 saques no dia e com um limite de R$500. Se o usuário não tiver saldo, cabe ao sistema avisá-lo. Os saques devem ser armazenados em uma variável.                       
+4. No extrato devem estar listados todos os saques e depósitos. No final, deve conter o saldo atual.        
--- sistema-bancario.py
+++ sistema-bancario.py
@@ -4,6 +4,11 @@ LIMITE_SAQUE = 500
 quantia_saque = 0
 saques_feitos = 0
 
+quantia_deposito = 0
+depositos_feitos = 0
+
+
+
 #Variável Menu para orientar o cliente
 menu = """
 Bem-vindo ao menu de nosso sistema bancário. Leia todas as funcionalidades abaixo:
@@ -11,28 +16,50 @@ Bem-vindo ao menu de nosso sistema bancário. Leia todas as funcionalidades abai
 (s) para realizar saques.
 (d) para realizar depósitos.
 (e) para conferir o extrato.
-(s) para sair.
+(q) para sair.
 
 Insira sua opção: 
 """
-opcao = input(menu)
+
+op = "l"
 
 while True:
-
+    opcao = input(menu)
+    
     if opcao == 's':
         saque = float(input("Insira a quantidade que deseja sacar: "))
     #Fazendo as restrições do saque   
         if saque > 500  or quantia_saque == 500:
-            print("O limite de saque é de 500 reais por dia...")
+            print("O limite de saque é de 500 reais por dia...", end = "\n")
+            continue
         elif saque > saldo:
-            print("Você não possui saldo suficiente para realizar esse saque. Por favor, tente novamente...")
+            print("Você não possui saldo suficiente para realizar esse saque. Por favor, tente novamente...", end = "\n")
+            continue
         elif saques_feitos == 3:
-            print("Você ja realizou o máximo de saques no dia...")
+            print("Você ja realizou o máximo de saques no dia que são 3...", end = "\n")
+            continue
         else:
             quantia_saque = quantia_saque + saque
             saques_feitos += 1
-        continue 
+            saldo = saldo - saque
+        
     elif opcao == 'd':
         deposito = float(input("Informe a quantia que quer depositar: "))
         saldo = saldo + deposito
 
+        quantia_deposito = quantia_deposito + deposito
+        depositos_feitos += 1
+    
+    elif opcao == "e":
+        extrato = f"""----------EXTRATO BANCÁRIO----------\n 
+Saldo Atual da conta: {saldo}
+Total de Saques efetuados: {saques_feitos}
+Valor total sacado: {quantia_saque}
+Total de depositos efetuados: {depositos_feitos}
+Valor total depositado: {quantia_deposito}\n 
+            """
+        print(extrato)
+    elif opcao == "q":
+        break
+
+print(saldo)
